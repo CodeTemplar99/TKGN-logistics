a0cefcbd1bd4fdcbe902eed130acac9e6c3f33da
CodeTemplar99 <udoagwac@gmail.com>
2020-08-03 19:47:13 +0100

    set background. 2nd commit

---
+++ js/spine.js
@@ -0,0 +1,18 @@
+function scrollToService() {
+    document.getElementById("service-view").scrollIntoView();
+    console.log("scrolled")
+}
+
+
+function scrollToInfo() {
+    document.getElementById("about-view").scrollIntoView();
+    console.log("scrolled")
+}
+
+function scrollToContact() {
+    document.getElementById("contact-view").scrollIntoView();
+    console.log("scrolled")
+}
+// document
+//     .getElementById('kristen-wiig')
+//     .scrollIntoView({ behavior: 'smooth' });
